d3e67eb921dbc27a5f68ebbc6ee1a6398b47cb88
patrickfitzgerald <fitzgepc@mail.uc.edu>
2026-02-13 17:48:29 -0500

    Final Commit plus psuedo code

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,19 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main
+
+    //class birthMonth
+        //main()
+            //num birthMonth = 0
+            //output "Enter your birth month [1-12]: "
+            //input birthMonth
+                //if (birthMonth >= 1 && birthMonth <= 12) then
+                    //output "Your birth month is: " + birthMonth
+                //else
+                    //output "You entered an incorrect month value: " + birthMonth
+                //endIf
+            //retun
+    //endClass
 {
     public static void main(String[] args)
     {
